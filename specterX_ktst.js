c608358e6aa49c16dd926695293dd5e113cc8632
htawa <robo_2nd@hotmail.com>
2017-07-03 23:06:19 +0900

    specterX ktst

--- specterX_ktst.js
+++ specterX_ktst.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         specterX ktst
 // @namespace    miu$_specterX_ktst
-// @version      0.1.7
+// @version      0.1.8
 // @description  ktstのログに色々書き加えていくスクリプト。
 // @author       ssz
 // @match        http://lisge.com/kk/k/*
